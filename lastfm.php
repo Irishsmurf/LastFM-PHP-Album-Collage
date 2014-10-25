db0dbf5fa45692d8eb4fa360309b199562a5490f
Paddez <paddez@redbrick.dcu.ie>
2014-10-25 16:18:11 +0000

    GetJSON

--- lastfm.php
+++ lastfm.php
@@ -29,120 +29,41 @@ include('aws-autoloader.php');
 
 use Aws\S3\S3Client;
 
+function getJson($url)
+{
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_USERAGENT, 'www.paddez.com/lastfm/');
+    $response = curl_exec($curl);
+
+    if($response == false)
+    {
+        $info = curl_getinfo($curl);
+        curl_close($curl);
+        die('Error: '.var_export($info));
+    }
+
+    curl_close($curl);
+    $decoded = json_decode($reponse);
+    return $decoded;
+}
+
 $s3 = S3Client::factory(array(
     'key' => $config['accessKey'],
     'secret' => $config['secretKey']));
-
-$result = $s3->listObjects(array(
-    'Bucket' => 'lastfm-img-paddez'));
-
-foreach($result as $object)
-{
-    echo $object['Key']."\n";
-}
 
 $lastfmApi = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=disco&api_key=".$config['api_key']."&format=json";
 
 $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
 $url = substr($url, strpos($url, '?')+1);
+
 //Parses the $vars and assigns the values as in the URL. $name and $period expected here.
 parse_str($url);
-$name = strtolower($name);
-$period = strtolower($period);
-$m = $width*$length;
-//Filepath to the Cached Images.
-$path = "i/php/$name$period$m.jpg";
+$request['name'] = strtolower($name);
+$request['period'] = strtolower($period);
+$request['width'] = $width;
+$request['length'] = $length;
+
 $noimage = "http://cdn.last.fm/flatness/catalogue/noimage/2/default_album_medium.png";
-//File Modified date
-$time = filemtime($path);
-//Checks if a file exists and is older than 2 hours. If so it outputs the image in the cache
 
-$lastfmApi = "http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&user=$name&period=$period&api_key=".$config['api_key']."&format=json";
-
-if(file_exists($path) && $time > time() - 18000 )
-{
-	header ("Content-type: image/jpeg");
- 	header("Cache-Control: must-revalidate");
-	$offset = 60 * 10;
- 	$ExpStr = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";
- 	header($ExpStr);
-	readfile($path);	
-}
-else if($time < time() - 18000) //Or else it fetches a new image
-{
-	//Include a User Agent for the XML Query
-	header("User-Agent" . ": Last.fm/Album Collage");
-    $user_url = $lastfmApi;
-    echo $user_url;
-	$ch = curl_init();
-	//Large Images are defined by this tag.
-	$needle = "<image size=\"large\">";
-	$endneedle = "</image>";
-	$rowcount = 0;
-	//Follows the X & Y axis
-	$loc = 0;
-	$loc2 = 0;
-	//Grabs the XML page via a proxy.
-	$last_xml = proxy_url($user_url);
-	$counter = 0;
-	//The Resized image at 300x300 pixels.
-	$resize = imagecreatetruecolor(100*$width, 100*$length);
-	$myImg = imagecreatetruecolor(126*$width, $length*126);
-	$bg = imagecolorallocate($myImg, 255, 255, 255);
-	imagefill($myImg, 0, 0, $bg);
-	imagefill($resize, 0, 0, $bg);
-	//3x3 Albums = 9 Images to be grabbed.
-	while($counter != $width*$length && $counter < 37 )
-	{
-		//Grabs the URL for the Images
-		$start = strpos($last_xml, $needle);
-		$last_xml = substr($last_xml, $start + 20 );
-		$end = strpos($last_xml, $endneedle);
-		$imgurl = substr($last_xml, 0, strpos($last_xml, $endneedle));
-	//Check is there's an actual image there, if not skip this turn, and get next album
-		if(strcmp($noimage, $imgurl) != 0)
-		{	
-			//Saves the images according to its file type.
-			if(strcmp (substr($imgurl, -3), "jpg" ) == 0)
-			{
-				save_image($imgurl, "topalbum.jpg");
-				$image = imagecreatefromjpeg("topalbum.jpg");
-			}
-			if(strcmp(substr($imgurl, -3), "png" ) == 0)
-			{
-				save_image($imgurl, "topalbum.png");
-				$image = imagecreatefrompng("topalbum.png");
-			}
-			//If the X axis reaches the end of the 3x3 grid, change Y by one increment of 126 and reset X
-			if ($loc == 126*$width)
-			{
-				$loc = 0;
-				$loc2 = $loc2 + 126;
-			}
-		
-			imagecopy($myImg, $image, $loc, $loc2, 0, 0, 126, 126);
-			//Gets rid of the Albums processed ready for the next Album.
-			$last_xml = substr($last_xml, strpos($last_xml, $endneedle));
-			$counter++;
-			//Increments X
-			$loc = $loc + 126;
-		}
-	}
-	header ("Content-type: image/jpeg");//Send type tobrowser
- 	header("Cache-Control: must-revalidate");//Set Cahce
-	$offset = 60 * 10;
- 	$ExpStr = "Expires: " . gmdate("D, d M Y H:i:s", time() + $offset) . " GMT";//Expire date
- 	header($ExpStr);
-
-	//save the jpg resize to the path!
-	imagecopyresampled( $resize, $myImg, 0, 0, 0, 0, 100*$width, 100*$length, 126*$width, 126*$length);
-	imagejpeg($resize, $path, 100);
-	chmod($path, 0644); //Make it readable!
-	readfile($path);//Read the file and show to the request!
-	
-	//Cleam up tiem!
-	imagedestroy($myImg);
-	imagedestroy($image);
-	imagedestroy($resize);
-}
 ?>
