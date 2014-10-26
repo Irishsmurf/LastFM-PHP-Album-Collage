0a29f5d76353540afcec47bcce0c7ff86b2c3588
Paddez <paddez@redbrick.dcu.iem>
2014-10-26 17:12:51 +0000

    Width & Length change

--- lastfm.php
+++ lastfm.php
@@ -171,8 +171,8 @@ $url = substr($url, strpos($url, '?')+1);
 
 //Parses the $vars and assigns the values as in the URL. $name and $period expected here.
 parse_str($url);
-$width = 3;
-$length = 3;
+//$width = 3;
+//$length = 3;
 $request['user'] = $user;
 $request['period'] = $period;
 $request['width'] = $width;
