cf5d9662b32fb4ca4c347fb9de1501b6b3041d26
Dave K <dave@paddez.com>
2015-06-27 12:12:19 +0200

    As requested - Playcount http://boards.4chan.org/mu/thread/56863719/10x3-thread-squares-are-the-worst-shape#p56864342

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ img{
 if(isset($_POST['name']) && isset($_POST['period']) && isset($_POST['width']) || !empty($_POST)){
 //	echo "<a href=\"http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."lastfm.php?user=".$_POST['name']."&period=".$_POST['period']."&cols=".$_POST['width']."&rows=".$_POST['len']."&info=".$_POST['info']."\">\n";
 	echo '<a href="http://content.paddez.com/images/'.strtolower($_POST['name']).'-'.$_POST['period'].'.jpg">';	
-	echo "<img src=\"http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."lastfm.php?user=".$_POST['name']."&period=".$_POST['period']."&cols=".$_POST['width']."&rows=".$_POST['len']."&info=".$_POST['info']."\"></img></a>\n";
+	echo "<img src=\"http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']."lastfm.php?user=".$_POST['name']."&period=".$_POST['period']."&cols=".$_POST['width']."&rows=".$_POST['len']."&info=".$_POST['info']."&playcount=".$_POST['playcount']."\"></img></a>\n";
 }
 else {
 	echo "<img src=\"https://static.paddez.com/images/notload.gif\"></img>\n";
@@ -127,7 +127,7 @@ for($x=4; $x<=15; $x++){
 </tr>
 <td class="label"> Playcount: </td>
 <td>
-<input type="checkbox" name="playcount" value="0">
+<input type="checkbox" name="playcount" value="1">
 </td>
 </tr>
 </table>
