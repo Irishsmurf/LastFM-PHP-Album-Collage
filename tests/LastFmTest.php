004f2b7c0cdc658f747d3fbccc99c322df9f5074
Irishsmurf <dave@paddez.com>
2016-02-06 21:10:20 +0000

    Duh PHP Fail

--- tests/LastFmTest.php
+++ tests/LastFmTest.php
@@ -42,27 +42,27 @@ class StubCurl extends Curl {
 class CurlTest extends PHPUnit_Framework_TestCase{
   private $curl = null;
   public function setUp(){
-    $this->$curl = new Curl();
+    $this->curl = new Curl();
   }
 
   public function testGetBody(){
-    $body = $this->$curl->getBody();
+    $body = $this->curl->getBody();
     $this->assertNull($body);
   }
 
   public function testSetData_string(){
-    $instance = $this->$curl->setData("Test Data");
+    $instance = $this->curl->setData("Test Data");
     $this->assertEquals($instance->getData(), "Test Data");
   }
 
   public function testSetData_array(){
     $array = array("some", "array");
-    $instance = $this->$curl->setData($array);
+    $instance = $this->curl->setData($array);
     $this->assertEquals($instance->getData(), http_build_query($array));
   }
 
   public function testGetStatus(){
-    $status = $this->$curl->getStatusCode();
+    $status = $this->curl->getStatusCode();
     $this->assertNull($status);
   }
 }
