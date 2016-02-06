a9d497870e8c5493f1b6d9042108daba24bc8aa1
Irishsmurf <dave@paddez.com>
2016-02-06 21:06:01 +0000

    Adding curl.php tests

--- tests/LastFmTest.php
+++ tests/LastFmTest.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'lib/Utils.php';
+require_once 'lib/curl.php';
 
 class StubCurl extends Curl {
   public function _construct() {
@@ -38,6 +39,33 @@ class StubCurl extends Curl {
 
 }
 
+class CurlTest extends PHPUnit_Framework_TestCase{
+  private $curl = null;
+  public function setUp(){
+    $this->$curl = new Curl();
+  }
+
+  public function testGetBody(){
+    $body = $this->$curl->getBody();
+    $this->assertNull($body);
+  }
+
+  public function testSetData_string(){
+    $instance = $this->$curl->setData("Test Data");
+    $this->assertEquals($instance->getData(), "Test Data");
+  }
+
+  public function testSetData_array(){
+    $array = array("some", "array");
+    $instance = $this->$curl->setData($array);
+    $this->assertEquals($instance->getData(), http_build_query($array));
+  }
+
+  public function testGetStatus(){
+    $status = $this->$curl->getStatusCode();
+    $this->assertNull($status);
+  }
+}
 class LastFmTest extends PHPUnit_Framework_TestCase {
   private $utils = null;
   public function setUp() {
