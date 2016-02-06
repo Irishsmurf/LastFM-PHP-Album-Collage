bf5012f7fb8cdc63bcf704bf5776d611b43b9930
Irishsmurf <dave@paddez.com>
2016-02-06 21:14:23 +0000

    Split tests

---
+++ tests/CurlTest.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'lib/curl.php';
+
+class CurlTest extends PHPUnit_Framework_TestCase{
+  private $curl = null;
+  public function setUp(){
+    $this->curl = new Curl();
+  }
+
+  public function testGetBody(){
+    $body = $this->curl->getBody();
+    $this->assertNull($body);
+  }
+
+  public function testSetData_string(){
+    $instance = $this->curl->setData("Test Data");
+    $this->assertEquals($instance->getData(), "Test Data");
+  }
+
+  public function testSetData_array(){
+    $array = array("some", "array");
+    $instance = $this->curl->setData($array);
+    $this->assertEquals($instance->getData(), http_build_query($array));
+  }
+
+  public function testGetStatus(){
+    $status = $this->curl->getStatusCode();
+    $this->assertNull($status);
+  }
+}
+
+?>
--- tests/LastFmTest.php
+++ tests/LastFmTest.php
@@ -39,33 +39,6 @@ class StubCurl extends Curl {
 
 }
 
-class CurlTest extends PHPUnit_Framework_TestCase{
-  private $curl = null;
-  public function setUp(){
-    $this->curl = new Curl();
-  }
-
-  public function testGetBody(){
-    $body = $this->curl->getBody();
-    $this->assertNull($body);
-  }
-
-  public function testSetData_string(){
-    $instance = $this->curl->setData("Test Data");
-    $this->assertEquals($instance->getData(), "Test Data");
-  }
-
-  public function testSetData_array(){
-    $array = array("some", "array");
-    $instance = $this->curl->setData($array);
-    $this->assertEquals($instance->getData(), http_build_query($array));
-  }
-
-  public function testGetStatus(){
-    $status = $this->curl->getStatusCode();
-    $this->assertNull($status);
-  }
-}
 class LastFmTest extends PHPUnit_Framework_TestCase {
   private $utils = null;
   public function setUp() {
